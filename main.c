7607ac01219d6f0242490dd8743aa54119b0992d
Harin23 <63739311+Harin23@users.noreply.github.com>
2026-06-27 21:47:36 -0600

    refactor and use int function for getting input for double

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_int_in_range(int min, int max, int *out) {
+int read_number_in_range(bool convertingInt, int min, int max, void *out) {
     if (min > max) {
         printf("ERROR: min must not be greater than max\n");
         return 0;
@@ -10,8 +10,6 @@ int read_int_in_range(int min, int max, int *out) {
     char input[256];
 
     while (1) {
-        printf("Please enter a valid number between %d and %d\n", min, max);
-
         if (!fgets(input, sizeof(input), stdin)) return 0;
 
         char *p_err;
@@ -19,11 +17,13 @@ int read_int_in_range(int min, int max, int *out) {
 
 
         if (v > max || v < min || p_err == input || (*p_err != '\0' && *p_err != '\n')) {
-            printf("Invalid\n\n");
+            printf("Invalid. Try again\n");
             continue;
         }
 
-        *out = (int) v;
+        if (convertingInt) *(int *) out = (int) v;
+        else *(double *) out = (double) v;
+
         return 1;
     }
 }
@@ -32,12 +32,15 @@ int main() {
     double scores[100];
 
     int count = 0;
-    printf("how many scores do you want to enter? (1–100)\n");
+    printf("How many scores do you want to enter? (1–100)\n");
 
-    if (!read_int_in_range(1, 100, &count)) return 0;
+    read_number_in_range(1, 1, 100, &count);
 
-    // for (int i = 0; i < count; i++) {
-    //
-    // }
+    for (int i = 0; i < count; i++) {
+        printf("Enter score #%d:\n", i + 1);
+        read_number_in_range(0, 1, 100, &scores[i]);
+        // printf("%d: %.10f\n", i, scores[i]);
+    }
+
     return 0;
 }
